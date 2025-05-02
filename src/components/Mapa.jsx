66f6b331f8a8740302c42bd27e91e7044cd684e2
Mauricio Rocha <jmauriciorocha13@gmail.com>
2025-05-02 13:54:55 -0700

    Mapa con indicaciones

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -1,41 +1,71 @@
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { GoogleMap, useJsApiLoader, Marker, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
   height: '500px',
 };
 
-const center = {
+const origen = {
   lat: 23.231541418530608,
   lng: -106.42652947574959,
 };
 
-function Mapa() {
-  const [selected, setSelected] = useState(null);
+const destino = {
+  lat: 23.198333582642356, 
+  lng: -106.42317927806837,
+};
 
-  return (
-    <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
-            <Marker position={center} onClick={() => setSelected(center)} />
-            {selected && (
-                <InfoWindow position={selected} onCloseClick={() => setSelected(null)}>
-                    <div style={{
-                        backgroundColor: 'white',
-                        padding: '10px',
-                        borderRadius: '8px',
-                        color: 'black',
-                        fontSize: '16px',
-                        maxWidth: '200px'
-                        }}>
-                        <h2>UAS</h2>
-                        <p style={{ margin: 0 }}>Facultad de Informática Mazatlán</p>
-                    </div>
-                </InfoWindow>
-            )}
-        </GoogleMap>
-    </LoadScript>
+const Mapa = () => {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyAWiyCiLAmLZz4rXarhHiXGAJJN2ym80uE',
+    libraries: ['places']
+  });
+
+  const [directions, setDirections] = useState(null);
+
+  const directionsCallback = ( response ) => {
+    if(response !== null) {
+      if(response.status === 'OK') {
+        setDirections(response);
+      } else {
+        console.error('Error al obtener direcciones:', response);
+      }
+    }
+  };
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={origen}
+      zoom={10}
+    >
+      {/* Mostrar la ruta */}
+      <DirectionsService
+        options={{
+          destination: destino,
+          origin: origen, 
+          travelMode: 'WALKING'
+        }}
+        callback={directionsCallback}
+      />
+
+      {directions && (
+        <DirectionsRenderer
+          options={{
+            directions: directions,
+          }}
+        />
+      )}
+
+      {/* Mostar marcadores */}
+      <Marker position={origen} label="UAS" />
+      <Marker position={destino} label="Machado" />
+
+    </GoogleMap>
+  ) : (
+    <div>Cargando mapa...</div>
   );
-}
+};
 
 export default Mapa;
